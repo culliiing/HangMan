b81a92451ddd5e6ece6b3fe79fa4a2228b32b4c7
Chris <103900975+Blissling@users.noreply.github.com>
2022-09-01 18:13:53 +0200

    Adding game-loop, replacing censored letters with guessed letters,

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -7,11 +7,40 @@ namespace HangMan
     class Program
     {
         // Turn all letters in any string into underscores
-        public static StringBuilder HideWord(string word)
+        //public static StringBuilder HideWord(string word)
+        //{
+        //    StringBuilder hiddenWord = new StringBuilder();
+
+        //    foreach (char c in word)
+        //    {
+        //        if (c != char.Parse(" "))
+        //        {
+        //            hiddenWord.Append("_");
+        //        }
+        //        else
+        //        {
+        //            hiddenWord.Append(' ');
+        //        }
+        //    }
+        //    return hiddenWord;
+        //}
+
+        static void Main(string[] args)
         {
+            int wrongGuesses = 0;
+            int guesses = 0;
+            string guess;
+
+            List<string> wordList = new List<string> {"Banana", "The United Kingdom", "Cow"};
+
+            // Pick a random word from the list (stored in winningWord)
+            Random random = new Random();
+            string winningWord = wordList[random.Next(wordList.Count)];
+
+            // Creating a censored version of the winningWord
             StringBuilder hiddenWord = new StringBuilder();
 
-            foreach (char c in word)
+            foreach (char c in winningWord)
             {
                 if (c != char.Parse(" "))
                 {
@@ -22,25 +51,39 @@ namespace HangMan
                     hiddenWord.Append(' ');
                 }
             }
-            return hiddenWord;
-        }
 
-        static void Main(string[] args)
-        {
-            int wrongGuesses = 0;
+            // Game loop
+            while (wrongGuesses < 3)
+            {
+                Console.WriteLine(winningWord);
+                Console.WriteLine(hiddenWord);
 
-            List<string> wordList = new List<string> {"Banana", "The United Kingdom", "Something"};
+                Console.WriteLine("Guess a letter!");
 
-            // Pick a random word from the list (stored in winningWord)
-            Random random = new Random();
-            string winningWord = wordList[random.Next(wordList.Count)];
+                // Taking the first character of the user's input
+                guess = Console.ReadLine();
+                char guessedLetter = guess[0];
 
-            Console.WriteLine(winningWord);
-            Console.WriteLine(HideWord(winningWord));
+                // Replace appropriate underscores with user's guessedLetter
+                for (int i = 0; i < winningWord.Length; i++)
+			    {
+                    if (guessedLetter == winningWord[i])
+                    {
+                        hiddenWord.Replace(char.Parse("_"), winningWord[i], i, 1);
+                    }
+			    }
 
-
-            //Console.WriteLine("Guess a letter!");
-            //Console.WriteLine(HideWord(winningWord));
+                // Inform user and register result
+                if (winningWord.Contains(guessedLetter))
+                {
+                    Console.WriteLine("Your letter is in the word.");
+                }
+                else
+	            {
+                    Console.WriteLine("Your letter is not in the word.");
+                    wrongGuesses++;
+	            }
+            }
 
             //while (wrongGuesses <= 6)
             //{
